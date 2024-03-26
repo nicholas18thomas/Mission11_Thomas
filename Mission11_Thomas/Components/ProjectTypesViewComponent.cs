dafad7238341eca07f605a567fec4673e472db56
nicholas18thomas <nicholas18thomas@gmail.com>
2024-03-26 10:11:36 -0600

    all good

---
+++ Mission11_Thomas/Components/ProjectTypesViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission11_Thomas.Models;
+
+namespace Mission11_Thomas.Components
+{
+    public class ProjectTypesViewComponent : ViewComponent
+    {
+        private IBookRepository _repo;
+
+        //Constructor
+        public ProjectTypesViewComponent(IBookRepository temp) 
+        {
+            _repo = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var bookTypes = _repo.Books
+                .Select(x => x.Title)
+                .Distinct()
+                .OrderBy(x => x)
+                .ToList();
+
+            return View(bookTypes);
+        }
+    }
+}
